16cfaf45935bdbcd39dfa95a606d20358fb86c87
kouhin <kou.h@ctw.inc>
2022-11-01 18:14:00 +0900

    chore: update webpack and fix errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,42 @@
 const fs = require("fs");
 const webpack = require("webpack");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const path = require("path");
 
 const useMinification = process.env.BUILD_MINIFIED === "true";
-const packageName = JSON.parse(fs.readFileSync("package.json", "utf8")).name.replace(/^@.*\//, "");
-const libraryName = packageName.replace(/(^|-)(.)/g, (match, _, c) => (c ? c.toUpperCase() : ""));
+const packageName = JSON.parse(
+  fs.readFileSync("package.json", "utf8")
+).name.replace(/^@.*\//, "");
+const libraryName = packageName.replace(/(^|-)(.)/g, (match, _, c) =>
+  c ? c.toUpperCase() : ""
+);
 
+/** @type {import('webpack').Configuration} */
 const config = {
   entry: "./src/index.ts",
   resolve: { extensions: [".ts", ".js"] },
   module: {
     rules: [
-      { test: /\.ts$/, loader: "awesome-typescript-loader", exclude: "/node_modules/" },
+      {
+        test: /\.ts$/,
+        loader: "awesome-typescript-loader",
+        exclude: "/node_modules/",
+      },
     ],
   },
   output: {
-    filename: `dist/${packageName}.js`,
+    path: path.resolve(__dirname, "dist"),
+    filename: useMinification ? `${packageName}.min.js` : `${packageName}.js`,
     libraryTarget: "umd",
     library: libraryName,
+  },
+  optimization: {
+    minimize: useMinification,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
   },
   plugins: [
     new webpack.DefinePlugin({
@@ -28,34 +47,6 @@ const config = {
   ],
 };
 
-const minified = {
-  ...{},
-  ...config,
-  ...{
-    output: {
-      ...{},
-      ...config.output,
-      ...{
-        filename: `dist/${packageName}.min.js`,
-      },
-    },
-    optimization: {
-      minimizer: [
-        new UglifyJsPlugin({
-          uglifyOptions: {
-            output: {
-              comments: false,
-            },
-            sourceMap: false,
-          },
-        }),
-      ],
-    },
-  },
-};
-
 // @TODO let webpack run configurations in parallel after solving:
 // https://github.com/s-panferov/awesome-typescript-loader/issues/323
-module.exports = useMinification
-  ? minified
-  : config;
+module.exports = config;
